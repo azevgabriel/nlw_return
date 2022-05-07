573bd148ff2964d4104e13a94adcd782240dc86f
Gabriel Azevedo <azevgabriel@gmail.com>
2022-05-07 17:46:25 -0300

    feat: class Three

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,16 +9,6 @@ const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  if (!comment || !type)
-    return res
-      .status(400)
-      .json({ actiom: false, message: 'Comment and type are required' });
-
-  if (screenshot && !screenshot.match(/^data:image\/(png|jpeg);base64,/))
-    return res
-      .status(400)
-      .json({ actiom: false, message: 'Screenshot is not valid' });
-
   const prismaFeedbackRepository = new FeedbacksRepository();
   const nodemailerMailService = new MailService();
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
